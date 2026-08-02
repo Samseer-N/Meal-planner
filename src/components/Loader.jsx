da28e91991879afe088177053a57484ff58eb296
mahalax5 <maha.31.01.07@gmail.com>
2026-08-02 16:49:05 +0530

    loader done

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,5 +1,19 @@
 function Loader() {
-  return <h2>Loader</h2>;
+  const styles = {
+    loader: {
+      textAlign: "center",
+      fontSize: "22px",
+      color: "#ff6b35",
+      margin: "40px",
+      fontWeight: "bold",
+    },
+  };
+
+  return (
+    <div style={styles.loader}>
+      Loading Meals...
+    </div>
+  );
 }
 
 export default Loader;
